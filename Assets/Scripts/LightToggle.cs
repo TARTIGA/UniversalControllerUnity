a44a07698d0dca3a32d7a4c79b8676bac11c4cf5
Tim <timigntrt@gmail.com>
2020-06-12 19:04:08 +0300

    init2

---
+++ Assets/Scripts/LightToggle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour
+{
+    public GameObject DirectionLight;
+    public bool toggleValue;
+    // Start is called before the first frame update
+
+    public void LightToggleHandler()
+    {
+        DirectionLight.SetActive(!toggleValue);
+        toggleValue = !toggleValue;
+    }
+}
---
+++ Assets/UniversalController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalController : MonoBehaviour
+{
+
+    int leftFingerId, rightFingerId;
+    float halfScreenWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // id = -1 finger not tracked
+        leftFingerId = -1;
+        rightFingerId = -1;
+
+        //get halfScreen
+        halfScreenWidth = Screen.width / 2;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Iterate all touches
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch t = Input.GetTouch(i);
+
+            //Cheach each touch`s phase
+            switch (t.phase)
+            {
+                case TouchPhase.Began:
+                    if (t.position.x < halfScreenWidth && leftFingerId == -1)
+                    {
+                        //Start tracking the left finger if it was not previously being traked
+                        leftFingerId = t.fingerId;
+                        Debug.Log("Tracking Left F");
+                    }
+                    else if (t.position.x > halfScreenWidth && rightFingerId == -1)
+                    {
+                        //Start tracking the right finger if it was not previously being traked
+                        rightFingerId = t.fingerId;
+                        Debug.Log("Tracking Right F");
+                    }
+                    break;
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (t.fingerId == leftFingerId)
+                    {
+                        //Stop tracking Left finger
+                        leftFingerId = -1;
+                        Debug.Log("Stopped tracking Left finger");
+                    }
+                    else if (t.fingerId == rightFingerId)
+                    {
+                        //Stop tracking Right finger
+                        rightFingerId = -1;
+                        Debug.Log("Stopped tracking Right finger");
+                    }
+                    break;
+
+            }
+        }
+
+    }
+}
